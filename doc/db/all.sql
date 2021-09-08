07bf42f4215e5484a7d75f895627d7b8204e5b2e
ZhenyeLee <73271404+ZhenyeLee@users.noreply.github.com>
2021-09-08 15:23:28 -0500

    add Chapter table using mybatis generator

--- doc/db/all.sql
+++ doc/db/all.sql
@@ -1,3 +1,12 @@
+drop table if exists `chapter`;
+create table `chapter` (
+                           `id` char(8) not null comment 'ID',
+                           `course_id` char(8) comment 'Course ID',
+                           `name` varchar(50) comment 'name',
+                           primary key (`id`)
+) engine=innodb default charset=utf8mb4 comment='chapter';
+
+
 drop table if exists `test`;
 create table `test` (
                         `id` char(8) not null default '' comment 'id',
